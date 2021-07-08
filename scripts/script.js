55fe2d8dda56940bf702e5acfe2555acc4722b79
Sergey6 <stankov6@gmail.com>
2021-07-08 20:03:54 +0300

    28 min 17 sec

--- scripts/script.js
+++ scripts/script.js
@@ -69,9 +69,7 @@ const getGoods = (callback) => {
       console.log(err);
     });
 };
-// getGoods((data) => {
-//   console.log(data);
-// });
+
 subheaderCart.addEventListener("click", cartModuleOpen);
 
 cartOverlay.addEventListener("click", (event) => {
@@ -87,18 +85,31 @@ try {
     throw "This is not a goods page!";
   }
   const createCard = (data) => {
+    const preview = data.preview;
+    const id = data.id;
+    const brand = data.brand;
+    const name = data.name;
+    const sizes = data.sizes;
+    const cost = data.cost;
+
     const li = document.createElement("li");
     li.classList.add("goods__item");
     li.innerHTML = `
      <article class="good">
-        <a class="good__link-img" href="card-good.html#id56454">
-            <img class="good__img" src="goods-image/AD002EMLUEA8_14164246_1_v1.jpg" alt="">
+        <a class="good__link-img" href="card-good.html#${id}">
+            <img class="good__img" src="goods-image/${preview}" alt="">
         </a>
         <div class="good__description">
-            <p class="good__price">2890 &#8381;</p>
-            <h3 class="good__title">Eazyway <span class="good__title__grey">/ Тайтсы</span></h3>
-            <p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">40 42 44 46</span></p>
-            <a class="good__link" href="card-good.html#id56454">Подробнее</a>
+            <p class="good__price">${cost} &#8381;</p>
+            <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+            ${
+              sizes
+                ? ` <p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">
+            ${sizes.join(" ")}</span></p>`
+                : ""
+            }
+           
+            <a class="good__link" href="card-good.html#${id}">Подробнее</a>
         </div>
       </article>
      `;
@@ -106,9 +117,12 @@ try {
     return li;
   };
   const renderGoodsList = (data) => {
-    console.log(data);
-    // goodsList.textContent = "";
-    // for (let i = 0; i < data.lenght; i++) {}
+    goodsList.textContent = "";
+
+    data.forEach((item) => {
+      const card = createCard(item);
+      goodsList.append(card);
+    });
   };
   getGoods(renderGoodsList);
 } catch (err) {
